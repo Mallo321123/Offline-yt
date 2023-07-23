1e2efadb6a9b3ed3bc76aecf78536f28cef12239
mallo321123 <franz.huber1@gmx.de>
2023-07-23 15:52:56 +0200

    downgrade

--- main.py
+++ main.py
@@ -23,34 +23,34 @@ options = {
 
 ydl = yt_dlp.YoutubeDL(options)
 
-def get_file_age():
-    try:
-        file_stats = os.stat(config['generell']['download_path'])
-        modification_time = file_stats.st_mtime
-        current_time = time.time()
-        age_seconds = current_time - modification_time
-        age_days = age_seconds / (60 * 60 * 24)
+#def get_file_age():
+#    try:
+#        file_stats = os.stat(config['generell']['download_path'])
+#        modification_time = file_stats.st_mtime
+#        current_time = time.time()
+#        age_seconds = current_time - modification_time
+#        age_days = age_seconds / (60 * 60 * 24)
         
-        return age_days
-    except FileNotFoundError:
-        return None
+#        return age_days
+#    except FileNotFoundError:
+#        return None
 
-def delete_old_videos():
-    for filename in os.listdir(config['generell']['download_path']):
-        file_path = os.path.join(config['generell']['download_path'], filename)
+#def delete_old_videos():
+#    for filename in os.listdir(config['generell']['download_path']):
+#        file_path = os.path.join(config['generell']['download_path'], filename)
         
-        if filename.lower().endswith(('.mp4', '.webm')):
-            age_in_days = get_file_age(file_path)
+#        if filename.lower().endswith(('.mp4', '.webm')):
+#            age_in_days = get_file_age(file_path)
 
-            if age_in_days is not None and age_in_days > config['generell']['history']:
-                os.remove(file_path)
-                print(f"Gelöscht: {filename}")
+#            if age_in_days is not None and age_in_days > config['generell']['history']:
+#                os.remove(file_path)
+#                print(f"Gelöscht: {filename}")
     
 while True:
     for i in range(0, len(urls)):
         url = urls[i]
         ydl.download(url)
         
-    delete_old_videos()
+    #delete_old_videos()
     
     time.sleep(config['generell']['intervall'])
